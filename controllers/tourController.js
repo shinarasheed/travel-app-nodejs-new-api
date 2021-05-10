2e58218743e78960a0693d307080982ddb4b6a72
shina <rasheedshinaopeyemi@gmail.com>
2021-05-10 16:26:38 +0100

    started on adding api features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,9 +2,27 @@ const Tour = require('../models/tour');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
-    //what's the difference from the above
-    //  const tours = await Tour.find({});
+    // const tours = await Tour.find();
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //BUILD THE QUERY
+    //copy the query parameters
+    const queryObj = { ...req.query };
+
+    //exclude the fields that we cannot query for in the documents
+    //but that we want to use to implement things like pagination, sorting
+    //limiting and returning specific fields
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
