72e19da9aba48c14d8e6557edd07dfd2b78bea1f
shina <rasheedshinaopeyemi@gmail.com>
2021-05-12 09:44:17 +0100

    implementing error handling

--- app.js
+++ app.js
@@ -21,4 +21,12 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//unhandled routes response
+app.all('*', (req, res, next) => {
+  res
+    .status(404)
+    .json({ status: 'fail', message: `Can't find ${req.originalUrl}` });
+  // next();
+});
+
 module.exports = app;
