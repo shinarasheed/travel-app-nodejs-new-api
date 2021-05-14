11b7678f95f8ac058bd9cbefb19a17495d378389
shina <rasheedshinaopeyemi@gmail.com>
2021-05-14 11:55:20 +0100

    implemented functionality to guard against NOSQL injection and xss atacks

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const connectDb = require('./config/db');
 
 //error handlers
@@ -17,6 +20,11 @@ connectDb();
 const app = express();
 
 //MIDDLEWARES
+
+//set secure http headers
+app.use(helmet());
+
+//development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -30,7 +38,15 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+//Bodyparser.  parse body data
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NOSQL query injection
+app.use(mongoSanitize);
+
+//Data sanitization against XSS
+app.use(xss());
+
 //serve static files
 app.use(express.static(`${__dirname}/public`));
 
