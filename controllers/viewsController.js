e77d0b268f6f5ce3096430c7b9ef6c2e113de35a
shina <rasheedshinaopeyemi@gmail.com>
2021-05-19 16:02:34 +0100

    added mapbox and tour location data

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -20,7 +20,7 @@ const getTour = catchAsync(async (req, res) => {
       "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
     )
     .render('tour', {
-      title: `${tour.title} Tour`,
+      title: `${tour.name} Tour`,
       tour,
     });
 });
