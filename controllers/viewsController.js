dc2658f5e011c2a6165ac9eaf729798a64027c1b
shina <rasheedshinaopeyemi@gmail.com>
2021-05-19 16:11:34 +0100

    added the login screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,7 +8,7 @@ const getOverView = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-const getTour = catchAsync(async (req, res) => {
+const getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     select: 'review rating user',
@@ -25,4 +25,8 @@ const getTour = catchAsync(async (req, res) => {
     });
 });
 
-module.exports = { getOverView, getTour };
+const getLoginForm = catchAsync(async (req, res, next) => {
+  res.status(200).render('login', { title: 'Log into your account' });
+});
+
+module.exports = { getOverView, getTour, getLoginForm };
